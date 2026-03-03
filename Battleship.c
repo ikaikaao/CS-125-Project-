d0fd004fa650206007ca54ced509920648a9b0ff
0rmal <aidanmonsen@gmail.com>
2026-03-03 11:27:29 -0700

    Update Battleship.c

--- Battleship.c
+++ Battleship.c
@@ -11,6 +11,9 @@ Resources:
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <ctype.h>
+
+void toProperCase(char *str);
 
 
 int main()
@@ -18,13 +21,77 @@ int main()
       char so1, so2, so3, sy1, sy2, sy3;
       int sx1, sx2, sx3;
 
-      printf("Enter the orientation of your 4 ship (v/h): ");
-      scanf(" %c", &so1);
+      // Ship 1
+      // Orientation
+      do 
+      {
+            printf("Enter the orientation of your 4 length ship (v/h): ");
+            scanf(" %c", &so1);
+            toProperCase(&so1);
 
-      printf("Enter the starting x coordinate of your 4 ship: ");
-      scanf("%d", &sx1);
-      // check to see if ship is horizontal and unable to fit on board
+            if ((so1 != 'V') && (so1 != 'H'))
+                  printf("Invalid orientation for ship!\n");
+
+      } while ((so1 != 'V') && (so1 != 'H'));
       
+      // X coordinate
+       do 
+       {
+            printf("Enter the starting x coordinate of your 4 length ship: ");
+            scanf("%d", &sx1);
+
+            if ((so1 == 'H') && (sx1 >= 4))
+                  printf("Invalid Position for ship!\n");
+
+      } while ((so1 == 'H') && (sx1 >= 4));
+      
+      // Y coordinate
+      do 
+      {
+            printf("Enter the starting y coordinate of your 4 length ship: ");
+            scanf(" %c", &sy1);
+            toProperCase(&sy1);
+
+            if ((so1 == 'V') && (sy1 >= 'C'))
+                  printf("Invalid Position for ship!\n");
+
+      } while ((so1 == 'V') && (sy1 >= 'C'));
+      
+      // Ship 2
+      // Orientation
+      do 
+      {
+            printf("Enter the orientation of your 3 length ship (v/h): ");
+            scanf(" %c", &so2);
+            toProperCase(&so2);
+
+            if ((so2 != 'V') && (so2 != 'H'))
+                  printf("Invalid orientation for ship!\n");
+
+      } while ((so2 != 'V') && (so2 != 'H'));
+      
+      // X coordinate
+      do 
+      {
+            printf("Enter the starting x coordinate of your 3 length ship: ");
+            scanf("%d", &sx2);
+
+            if ((so2 == 'H') && (sx2 >= 5))
+                  printf("Invalid Position for ship!\n");
+
+      } while ((so2 == 'H') && (sx2 >= 5));
+      
+      // Y coordinate
+      do 
+      {
+            printf("Enter the starting y coordinate of your 3 length ship: ");
+            scanf(" %c", &sy2);
+            toProperCase(&sy2);
+
+            if ((so2 == 'V') && (sy2 >= 'D'))
+                  printf("Invalid Position for ship!\n");
+
+      } while ((so2 == 'V') && (sy2 >= 'D'));
 }
 
 void toProperCase(char *str)
