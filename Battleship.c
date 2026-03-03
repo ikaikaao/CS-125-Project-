3cb223aa9a86f68502e8cca7bfe615abe0991974
0rmal <aidanmonsen@gmail.com>
2026-03-03 11:30:16 -0700

    Update Battleship.c

--- Battleship.c
+++ Battleship.c
@@ -1,7 +1,7 @@
 /*
 File: Battleship.c
 Authors: Aidan Monsen and Ikaikaokealohilani Ofsthun
-Purpose: Recreat Battleship in C
+Purpose: Recreate Battleship in C
 Version: 1.0 Feb 24, 2026
 Resources:
 */
