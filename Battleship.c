040be48276a1e6de68f09492cda9a214083aac23
0rmal <aidanmonsen@gmail.com>
2026-03-10 11:10:01 -0700

    Update Battleship.c

--- Battleship.c
+++ Battleship.c
@@ -2,7 +2,7 @@
 File: Battleship.c
 Authors: Aidan Monsen and Ikaikaokealohilani Ofsthun
 Purpose: Recreate Battleship in C
-Version: 1.0 Feb 24, 2026
+Version: 1.2 Mar 10, 2026
 Resources:
 */
 
@@ -15,83 +15,17 @@ Resources:
 
 void toProperCase(char *str);
 
+void getShip(int length, char *orientation, int *x, char *y);
 
 int main()
 {
       char so1, so2, so3, sy1, sy2, sy3;
       int sx1, sx2, sx3;
 
-      // Ship 1
-      // Orientation
-      do 
-      {
-            printf("Enter the orientation of your 4 length ship (v/h): ");
-            scanf(" %c", &so1);
-            toProperCase(&so1);
-
-            if ((so1 != 'V') && (so1 != 'H'))
-                  printf("Invalid orientation for ship!\n");
-
-      } while ((so1 != 'V') && (so1 != 'H'));
+      getShip(4, &so1, &sx1, &sy1);
+      getShip(3, &so2, &sx2, &sy2);
+      getShip(2, &so3, &sx3, &sy3);
       
-      // X coordinate
-       do 
-       {
-            printf("Enter the starting x coordinate of your 4 length ship: ");
-            scanf("%d", &sx1);
-
-            if ((so1 == 'H') && (sx1 >= 4))
-                  printf("Invalid Position for ship!\n");
-
-      } while ((so1 == 'H') && (sx1 >= 4));
-      
-      // Y coordinate
-      do 
-      {
-            printf("Enter the starting y coordinate of your 4 length ship: ");
-            scanf(" %c", &sy1);
-            toProperCase(&sy1);
-
-            if ((so1 == 'V') && (sy1 >= 'C'))
-                  printf("Invalid Position for ship!\n");
-
-      } while ((so1 == 'V') && (sy1 >= 'C'));
-      
-      // Ship 2
-      // Orientation
-      do 
-      {
-            printf("Enter the orientation of your 3 length ship (v/h): ");
-            scanf(" %c", &so2);
-            toProperCase(&so2);
-
-            if ((so2 != 'V') && (so2 != 'H'))
-                  printf("Invalid orientation for ship!\n");
-
-      } while ((so2 != 'V') && (so2 != 'H'));
-      
-      // X coordinate
-      do 
-      {
-            printf("Enter the starting x coordinate of your 3 length ship: ");
-            scanf("%d", &sx2);
-
-            if ((so2 == 'H') && (sx2 >= 5))
-                  printf("Invalid Position for ship!\n");
-
-      } while ((so2 == 'H') && (sx2 >= 5));
-      
-      // Y coordinate
-      do 
-      {
-            printf("Enter the starting y coordinate of your 3 length ship: ");
-            scanf(" %c", &sy2);
-            toProperCase(&sy2);
-
-            if ((so2 == 'V') && (sy2 >= 'D'))
-                  printf("Invalid Position for ship!\n");
-
-      } while ((so2 == 'V') && (sy2 >= 'D'));
 }
 
 void toProperCase(char *str)
@@ -102,4 +36,50 @@ void toProperCase(char *str)
           str[i] = tolower(str[i]);
       }
       str[0] = toupper(str[0]);
+}
+
+void getShip(int length, char *orientation, int *x, char *y)
+{
+
+      // Orientation
+      do 
+      {
+          printf("Enter the orientation of your %d length ship (v/h): ", length);
+          scanf(" %c", orientation);
+          toProperCase(orientation);
+
+          if ((*orientation != 'V') && (*orientation != 'H'))
+          {
+              printf("Invalid orientation for ship!\n");
+          }
+
+      } while ((*orientation != 'V') && (*orientation != 'H'));
+
+      // X coordinate
+      do 
+      {
+          printf("Enter the starting x coordinate of your %d length ship: ", length);
+          scanf("%d", x);
+
+          if (*x <= 0 || *x > BOARD_SIZE || (*orientation == 'H' && *x > BOARD_SIZE - length + 1))
+          {
+              printf("Invalid Position for ship!\n");
+          }
+
+      } while (*x <= 0 || *x > 6 || (*orientation == 'H' && *x > BOARD_SIZE - length + 1));
+      
+      // Y coordinate
+      do
+      {
+          printf("Enter the starting y coordinate of your %d length ship: ", length);
+          scanf(" %c", y);
+          toProperCase(y);
+
+          if (*y < 'A' || *y > 'A' + BOARD_SIZE - 1 || (*orientation == 'V' && *y > 'A' + BOARD_SIZE - length))
+          {
+              printf("Invalid Position for ship!\n");
+          }
+
+      } while (*y < 'A' || *y > 'A' + BOARD_SIZE - 1 || (*orientation == 'V' && *y > 'A' + BOARD_SIZE - length));
+
 }
