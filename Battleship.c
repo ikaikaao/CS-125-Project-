a2377e229788a5bb42282e7cdb0da600dfc7a488
0rmal <aidanmonsen@gmail.com>
2026-03-10 11:25:35 -0700

    Update Battleship.c

--- Battleship.c
+++ Battleship.c
@@ -15,17 +15,20 @@ Resources:
 
 void toProperCase(char *str);
 
-void getShip(int length, char *orientation, int *x, char *y);
+void getShip(int length, char *orientation, int *x, char *y, int board[BOARD_SIZE][BOARD_SIZE]);
 
 int main()
 {
       char so1, so2, so3, sy1, sy2, sy3;
       int sx1, sx2, sx3;
-
-      getShip(4, &so1, &sx1, &sy1);
-      getShip(3, &so2, &sx2, &sy2);
-      getShip(2, &so3, &sx3, &sy3);
       
+      int board[BOARD_SIZE][BOARD_SIZE] = {0};
+
+      getShip(4, &so1, &sx1, &sy1, board);
+      getShip(3, &so2, &sx2, &sy2, board);
+      getShip(2, &so3, &sx3, &sy3, board);
+
+      return 0;
 }
 
 void toProperCase(char *str)
@@ -38,48 +41,105 @@ void toProperCase(char *str)
       str[0] = toupper(str[0]);
 }
 
-void getShip(int length, char *orientation, int *x, char *y)
+void getShip(int length, char *orientation, int *x, char *y, int board[BOARD_SIZE][BOARD_SIZE])
 {
-
-      // Orientation
-      do 
-      {
-          printf("Enter the orientation of your %d length ship (v/h): ", length);
-          scanf(" %c", orientation);
-          toProperCase(orientation);
-
-          if ((*orientation != 'V') && (*orientation != 'H'))
-          {
-              printf("Invalid orientation for ship!\n");
-          }
-
-      } while ((*orientation != 'V') && (*orientation != 'H'));
-
-      // X coordinate
-      do 
-      {
-          printf("Enter the starting x coordinate of your %d length ship: ", length);
-          scanf("%d", x);
-
-          if (*x <= 0 || *x > BOARD_SIZE || (*orientation == 'H' && *x > BOARD_SIZE - length + 1))
-          {
-              printf("Invalid Position for ship!\n");
-          }
-
-      } while (*x <= 0 || *x > 6 || (*orientation == 'H' && *x > BOARD_SIZE - length + 1));
+      int i, overlap;
       
-      // Y coordinate
       do
       {
-          printf("Enter the starting y coordinate of your %d length ship: ", length);
-          scanf(" %c", y);
-          toProperCase(y);
+          overlap = 0;      
 
-          if (*y < 'A' || *y > 'A' + BOARD_SIZE - 1 || (*orientation == 'V' && *y > 'A' + BOARD_SIZE - length))
+          // Orientation
+          do 
           {
-              printf("Invalid Position for ship!\n");
+              printf("Enter the orientation of your %d length ship (v/h): ", length);
+              scanf(" %c", orientation);
+              toProperCase(orientation);
+
+              if ((*orientation != 'V') && (*orientation != 'H'))
+              {
+                  printf("Invalid orientation for ship!\n");
+              }
+
+          } while ((*orientation != 'V') && (*orientation != 'H'));
+      
+          // X coordinate
+          do 
+          {
+              printf("Enter the starting x coordinate of your %d length ship: ", length);
+              scanf("%d", x);
+
+              if (*x <= 0 || *x > BOARD_SIZE || (*orientation == 'H' && *x > BOARD_SIZE - length + 1))
+              {
+                  printf("Invalid Position for ship!\n");
+              }
+
+          } while (*x <= 0 || *x > 6 || (*orientation == 'H' && *x > BOARD_SIZE - length + 1));
+          
+          // Y coordinate
+          do
+          {
+              printf("Enter the starting y coordinate of your %d length ship: ", length);
+              scanf(" %c", y);
+              toProperCase(y);
+
+              if (*y < 'A' || *y > 'A' + BOARD_SIZE - 1 || (*orientation == 'V' && *y > 'A' + BOARD_SIZE - length))
+              {
+                  printf("Invalid Position for ship!\n");
+              }
+
+          } while (*y < 'A' || *y > 'A' + BOARD_SIZE - 1 || (*orientation == 'V' && *y > 'A' + BOARD_SIZE - length));
+
+          int row = *y - 'A';
+          int col = *x - 1;
+
+          // Check overlap
+          if (*orientation == 'H')
+          {
+              for (i = 0; i < length; i++)
+              {
+                  if (board[row][col + i] == 1)
+                  {
+                      overlap = 1;
+                  }
+              }
+          }
+          
+          else
+          {
+              for (i = 0; i < length; i++)
+              {
+                  if (board[row + i][col] == 1)
+                  {
+                      overlap = 1;
+                  }
+              }
           }
 
-      } while (*y < 'A' || *y > 'A' + BOARD_SIZE - 1 || (*orientation == 'V' && *y > 'A' + BOARD_SIZE - length));
+          if (overlap)
+          {
+              printf("Ships cannot overlap! Try again.\n");
+          }
+
+      } while (overlap);
+
+      // Mark ship on board
+      int row = *y - 'A';
+      int col = *x - 1;
+
+      if (*orientation == 'H')
+      {
+          for (i = 0; i < length; i++)
+          {
+              board[row][col + i] = 1;
+          }
+      }
+      else
+      {
+          for (i = 0; i < length; i++)
+          {
+              board[row + i][col] = 1;
+          }
+      }
 
 }
