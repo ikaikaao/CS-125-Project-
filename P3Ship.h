6bcbd88630e766218076ff6ab044e08416661df8
0rmal <aidanmonsen@gmail.com>
2026-03-24 11:18:43 -0700

    Create P3Ship.h

---
+++ P3Ship.h
@@ -0,0 +1,14 @@
+/*
+File: P3Ship.h
+Author: Aidan Monsen
+Purpose: Create a structure to track the players 3 length ships health
+Version: 1.0 Mar 24, 2026
+Resources:
+*/
+
+typedef struct
+{
+
+      int p[3] = {1, 1, 1};
+
+} P3Ship;
