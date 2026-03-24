94681ce87c3a01a9ba92c2748b22ee59239f6b88
0rmal <aidanmonsen@gmail.com>
2026-03-24 11:18:58 -0700

    Create P2Ship.h

---
+++ P2Ship.h
@@ -0,0 +1,14 @@
+/*
+File:  P2Ship.h
+Author: Aidan Monsen
+Purpose: Create a structure to track the players 2 length ships health
+Version: 1.0 Mar 24, 2026
+Resources: 
+*/
+
+typedef struct
+{
+
+      int p[2] = {1, 1};
+
+} P2Ship;
