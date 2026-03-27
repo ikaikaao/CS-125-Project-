3c759f6ea635592b9fac20c6079d36f241ccfd92
0rmal <aidanmonsen@gmail.com>
2026-03-27 13:35:41 -0700

    Create turnComp.h

---
+++ turnComp.h
@@ -0,0 +1,9 @@
+/*
+File: turnComp.h
+Author: Ikaikaokealohilani Ofsthun
+Purpose: header file to introduce the turnComp function
+Version: 1.0 Mar 27, 2026
+Resources:
+*/
+
+void turnComp(int Cboard[BOARD_SIZE][BOARD_SIZE]);
