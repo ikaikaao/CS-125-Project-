f5c28f31eb1888792060fff3ce9a278997564ad0
0rmal <aidanmonsen@gmail.com>
2026-03-27 13:31:46 -0700

    Create printEnemyBoard.h

---
+++ printEnemyBoard.h
@@ -0,0 +1,9 @@
+/*
+File: printEnemyBoard.h
+Author: Aidan Monsen
+Purpose: header to introduce the printEnemyBoard function
+Version: 1.0 Mar 25, 2026
+Resources:
+*/
+
+void printEnemyBoard(int Cboard[BOARD_SIZE][BOARD_SIZE]);
