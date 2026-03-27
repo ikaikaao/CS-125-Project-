60b7120444ba8483fc4965fa9703c9541c1e745c
0rmal <aidanmonsen@gmail.com>
2026-03-27 13:31:07 -0700

    Create getShip.h

---
+++ getShip.h
@@ -0,0 +1,9 @@
+/*
+File: getShip.h
+Author: Aidan Monsen
+Purpose: header file to introduce the getShip function
+Version: 1.0 Mar 25, 2026
+Resources:
+*/
+
+void getShip(int length, char *orientation, int *x, char *y, int Pboard[BOARD_SIZE][BOARD_SIZE]);
