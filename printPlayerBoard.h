bc7a0a37e53bf86de55df4a0f4f54cf64eee5c06
0rmal <aidanmonsen@gmail.com>
2026-03-27 13:32:04 -0700

    Create printPlayerBoard.h

---
+++ printPlayerBoard.h
@@ -0,0 +1,9 @@
+/*
+File: printPlayerBoard.h
+Author: Aidan Monsen
+Purpose: header file to introduce printPlayerBoard function
+Version 1.0 Mar 25, 2026
+Resources:
+*/
+
+void printPlayerBoard(int Pboard[BOARD_SIZE][BOARD_SIZE]);
