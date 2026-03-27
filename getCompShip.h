343f8eb6ba38b35255737315235f7c41cc400c85
0rmal <aidanmonsen@gmail.com>
2026-03-27 13:35:56 -0700

    Create getCompShip.h

---
+++ getCompShip.h
@@ -0,0 +1,9 @@
+/*
+File: getCompShip.h
+Author: Ikaikaokealohilani Ofsthun
+Purpose: header file to introduce the getCompShip function
+Version: 1.0 Mar 27, 2026
+Resources:
+*/
+
+void getCompShip(int Cboard[BOARD_SIZE][BOARD_SIZE]);
