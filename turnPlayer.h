4f4c9f6bf109ef078d2201d1c68cc69bffb2ea20
0rmal <aidanmonsen@gmail.com>
2026-03-31 11:48:03 -0700

    Create turnPlayer.h

---
+++ turnPlayer.h
@@ -0,0 +1,9 @@
+/*
+File: turnPlayer.h
+Author: Aidan Monsen
+Purpose: header file to introduce the turnPlayer function
+Version: 1.0 Mar 31, 2026
+Resources:
+*/
+
+void turnPlayer(int Cboard[BOARD_SIZE][BOARD_SIZE], int CPboard[BOARD_SIZE][BOARD_SIZE]);
