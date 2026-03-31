bf77d0a4368ac63278054ed78e0e9914b49d2a03
0rmal <aidanmonsen@gmail.com>
2026-03-31 11:48:24 -0700

    Create turnPlayer.c

---
+++ turnPlayer.c
@@ -0,0 +1,49 @@
+/*
+File: turnPlayer.c
+Author: Aidan Monsen
+Purpose: takes the players turn
+Version: 1.0 Mar 31, 2026
+Resources:
+*/
+
+#define BOARD_SIZE 6
+
+#include <stdio.h>
+
+void turnPlayer(int Cboard[BOARD_SIZE][BOARD_SIZE], int CPboard[BOARD_SIZE][BOARD_SIZE])
+{
+
+      int col;
+      char row;
+    
+      printf("PLAYER TURN\n\n");    
+    
+      printf("Enter the X coordinate (1-6) for your guess: ");
+      scanf("%d", &col);
+      
+      if ((col < 1) || (col > 6))
+      {
+          printf("Good Job\n");
+      }
+      
+      printf("Enter the Y coordinate (A-F) for your guess: ");
+      scanf(" %c", &row);
+
+      if ((row < 'A') || (row > 'F'))
+      {
+          printf("Learn English\n");
+      }
+      
+      int rowIndex = row - 'A';
+      int colIndex = col - 1;
+      
+      if (Cboard[rowIndex][colIndex] == 1)
+      {
+          CPboard[rowIndex][colIndex] = 1;
+      }
+      
+      else if (Cboard[rowIndex][colIndex] == 0)
+      {
+          CPboard[rowIndex][colIndex] = 2;
+      }
+}
