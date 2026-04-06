6645f6ded8bf8ae3bd7ec59281a5dbb544a34ade
0rmal <aidanmonsen@gmail.com>
2026-04-05 20:51:57 -0700

    Update turnPlayer.c

--- turnPlayer.c
+++ turnPlayer.c
@@ -35,6 +35,7 @@ int turnPlayer(int Cboard[BOARD_SIZE][BOARD_SIZE], int CPboard[BOARD_SIZE][BOARD
       
           printf("Enter the Y coordinate (A-F) for your guess: ");
           scanf(" %c", &row);
+          row = toupper(row);
 
           if ((row < 'A') || (row > 'F'))
           {
@@ -44,13 +45,35 @@ int turnPlayer(int Cboard[BOARD_SIZE][BOARD_SIZE], int CPboard[BOARD_SIZE][BOARD
       
           int rowIndex = row - 'A';
           int colIndex = col - 1;
-      
+
+          if (CPboard[rowIndex][colIndex] == 1 || CPboard[rowIndex][ColIndex] == 2)
+          {
+                printf("You already guessed that spot! Try again.\n");
+          }
+            
           if (Cboard[rowIndex][colIndex] == 1)
           {
               CPboard[rowIndex][colIndex] = 1;
               printf("\n(%d,%c)\n", col, row);
               printf("Hit, take another turn\n");
               printEnemyBoard(CPboard);
+
+              int hits = 0, r, c;
+              for (r = 0, r < BOARD_SIZE; r++)
+              {
+                    for (c = 0; c < BOARD_SIZE; c++)
+                    {
+                          if (CPboard[r][c] == 1)
+                          {
+                                hits++;
+                          }
+                    }
+              }
+              if (hits >= 9)
+              {
+                    return 2;
+              }
+  
               hit = 1;
           }
       
